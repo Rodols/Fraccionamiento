ef110f4891745f3a7f579773be6b9474d066583c
Rodo <vasquezbenitezrodolfo@gmail.com>
2019-02-15 20:56:26 -0600

    Se realiza cambio al menu en cuanto css y formato dejandolo funcional

--- plantillas/BarraNavegacion.inc.php
+++ plantillas/BarraNavegacion.inc.php
@@ -8,7 +8,7 @@ $tipo = $_SESSION['tipo'];
     			<span class="navbar-toggler-icon"></span>
 			</button>
 				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    				<a class="navbar-brand navbar-r" href="#">RealDelBosque</a>
+    				<h3>RealDelBosque </h3>
     			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 					<li class="nav-item "><a  href="registro_user.php" class=" btn btn-outline-success">
 						<i class="material-icons">&#xe55a;</i> <span> <?=$user?></span></a>
@@ -16,13 +16,13 @@ $tipo = $_SESSION['tipo'];
 					<li class="nav-item "><a  href="registro.php"  class="nav-link navbar-r btn btn-outline">
 						 <i class="material-icons">&#xe85d;</i> <span> RegistroDeEntrada</span></a>
       				 </li>
-      				<li class="nav-item "><a class="nav-link navbar-r btn btn-outline " href="consulta.php">
+      				<li class="nav-item "><a class="nav-link navbar-r  btn btn-outline " href="consulta.php">
 						  <i class="material-icons">&#xe862;</i><span>RegistroDeSalida</span></a>
      				 </li>
-					<li class="nav-item "><a  href="visitas.php" class="nav-link navbar-r btn btn-outline">
+					<li class="nav-item "><a  href="visitas.php" class="nav-link navbar-r  btn btn-outline">
 						 <i class="material-icons">&#xe2c9;</i> <span> Visitantes</span></a>
       				 </li>
-      				<li class="nav-item "><a class="nav-link navbar-r btn btn-outline " href="bitacora.php">
+      				<li class="nav-item "><a class="nav-link navbar-r  btn btn-outline " href="bitacora.php">
 						  <i class="material-icons">&#xe0e0;</i><span>Bitacora</span></a>
 					   </li>
 				 </ul>
