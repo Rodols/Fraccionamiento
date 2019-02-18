79f946d6ba0486f520d0ec367a9e76d5276511d9
Rodo <vasquezbenitezrodolfo@gmail.com>
2019-02-18 11:52:43 -0600

    Se coloco card de ayuda de busqueda en bitacora

--- php/bitacora.php
+++ php/bitacora.php
@@ -11,17 +11,30 @@ include_once '../plantillas/InicioDocumento.inc.php';
 <?php include_once '../plantillas/BarraNavegacion.inc.php';?>
 
 	<div class="container-fluid">
-		<center>
-			<h4 id="tabB"><br>Historial de visitas (Xalapa Ver, <?=$fechaActual;?>)</h4><br>
-				<form class="form-inline justify-content-center" id="formBuscarBita" method="POST" action="bitacora.php">
-  					<div class="form-group mb-2 ">
-    					<input type="text" class="form-control" name="busquedaB" id="buscarBitacora"  placeholder="Buscar">
+		<div class="row">
+			<div class="col-md-4"></div>
+			 <div class="col-md-4">
+				<center>
+					<h4 id="tabB"><br>Historial de visitas <br> (Xalapa Ver, <?=$fechaActual;?>)</h4><br>
+					<form class="form-inline justify-content-center" id="formBuscarBita" method="POST" action="bitacora.php">
+  						<div class="form-group mb-2 ">
+    						<input type="text" class="form-control" name="busquedaB" id="buscarBitacora"  placeholder="Buscar">
  		 				</div>
-  					<div class="form-group mx-sm-3 mb-2">
+  						<div class="form-group mx-sm-3 mb-2">
 							<button type="submit"  name="btnFiltro" id="btnfiltro" class="btn btn-info form-control">Buscar</button>
-				    </div>
-				</form>
-		</center>
+				    	</div>
+					</form>
+				</center>
+			</div>
+			<div class="col-md-4 ayuda">
+				<div class="card text-white bg-dark mb-2" style="max-width: 15rem;">
+  					<div class="card-header"><h5 class="card-title">¿Como buscar?</h5></div>
+  					<div class="card-body">
+    					<p class="card-text">Puedes "Buscar" por <span class="textAyuda"> codigo, usuario, fecha, nombre, placas.</span></p>
+ 					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	
 	<div class="container-fluid ">
